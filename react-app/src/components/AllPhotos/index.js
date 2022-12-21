ede9c4fc99002a150216b42be276dc284e52b5f8
jeydt23 <jeydt23@gmail.com>
2022-12-21 16:35:03 -0500

    allPhotos functionality working

--- react-app/src/components/AllPhotos/index.js
+++ react-app/src/components/AllPhotos/index.js
@@ -5,24 +5,26 @@ import { getAllPhotos } from '../../store/photo';
 
 
 function AllPhotos() {
-    const storiesState = useSelector(state => state.photo.viewAllPhotos)
+    const storiesState = useSelector(state => Object.values(state.photo.viewAllPhotos))
     const dispatch = useDispatch()
     useEffect(() => {
       dispatch(getAllPhotos())
 
-
     }, [dispatch]);
 
     if (!storiesState.length) return null;
+    console.log("storiesState ==== ", storiesState)
     return (
       <div>
         {storiesState.map(photo => (
-          <div key={photo.id}>
+
+          <div key={photo.User.id}>
+           { console.log("photo key ===== ", photo) }
             <h2>{photo.title}</h2>
             <p>{photo.description}</p>
             <img src={photo.file_path} alt={photo.title} />
             <p>Uploaded by {photo.User.username} on {photo.date_uploaded}</p>
-            <p>Tags: {photo.tags.join(', ')}</p>
+            {/* <p>Tags: {photo.tags.join(', ')}</p> */}
           </div>
         ))}
       </div>
--- react-app/src/store/photo.js
+++ react-app/src/store/photo.js
@@ -2,7 +2,7 @@ const LOAD_ALL_PHOTOS = '/photos/LOAD_ALL_PHOTOS'
 
 const allPhotos = photos => {
     return {
-        type: LOAD_ALL_PHOTOS
+        type: LOAD_ALL_PHOTOS, photos
     }
 }
 
@@ -11,6 +11,7 @@ export const getAllPhotos = () => async dispatch => {
     const response = await fetch(`/api/photos`)
     if (response.ok) {
         const photos = await response.json()
+        console.log("photos === ", photos)
         dispatch(allPhotos(photos))
         return photos
     }
@@ -21,9 +22,9 @@ export default function reducer(state = { viewOnePhoto: {}, viewAllPhotos: {} },
     switch (action.type) {
         case LOAD_ALL_PHOTOS: {
             const newState = { viewOnePhoto: {}, viewAllPhotos: {} }
-            action.photos.Photo.forEach(e => {
+            action.photos.Photos.forEach(e => {
                 console.log("e ===== ", e)
-                newState.allPhotos[e.id] = e
+                newState.viewAllPhotos[e.User.id] = e
             })
             return newState
         }
