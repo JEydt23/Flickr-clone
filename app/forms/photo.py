0d4a608025bf5cbf8957b88a227275743584e170
jeydt23 <jeydt23@gmail.com>
2022-12-21 12:15:38 -0500

    minor changes to photo seeds

--- app/forms/photo.py
+++ app/forms/photo.py
@@ -2,7 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms.fields import StringField
 from wtforms.validators import DataRequired
 
-class StoryForm(FlaskForm):
+class PhotoForm(FlaskForm):
     title = StringField("Title",[DataRequired()])
     description = StringField("Description",[DataRequired()])
     file_path = StringField("File_Path",[DataRequired()])
