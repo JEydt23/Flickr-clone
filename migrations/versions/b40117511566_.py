6d318efbba9c442576df4d472bcd1faef688743b
jeydt23 <jeydt23@gmail.com>
2023-01-03 18:38:00 -0500

    plz work

--- migrations/versions/b40117511566_.py
+++ migrations/versions/b40117511566_.py
@@ -13,7 +13,7 @@ environment = os.getenv("FLASK_ENV")
 SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
-revision = '28ca54290471'
+revision = 'b40117511566'
 down_revision = None
 branch_labels = None
 depends_on = None
