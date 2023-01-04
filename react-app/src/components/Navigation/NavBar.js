928afbe60b78a2ebcba6112ce25ce7417edefdb3
jeydt23 <jeydt23@gmail.com>
2023-01-04 02:20:55 -0500

    colors improved

--- react-app/src/components/Navigation/NavBar.js
+++ react-app/src/components/Navigation/NavBar.js
@@ -2,9 +2,17 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
 import logo from './flickr logo black.png'
+import session from '../../store/session'
+import { useSelector } from 'react-redux';
+
+
+
 import './NavBar.css'
 
+
+
 const NavBar = () => {
+  const user = useSelector(state => state.session.user)
   return (
     <nav>
       <div className='nav'>
@@ -13,23 +21,29 @@ const NavBar = () => {
             <img src={logo} alt='home' id='logo'></img><label>Picture This</label>
           </NavLink>
         </div>
-        <div>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </div>
-        <div>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </div>
-        {/* <div>
+        <div className='nav-right'>
+          <div >
+            <NavLink to='/login' exact={true} activeClassName='active' className='login'>
+              Login
+            </NavLink>
+          </div>
+          <div >
+            <button className='sign-button'>
+              <NavLink to='/sign-up' exact={true} activeClassName='active' className='sign-up'>
+                Sign Up
+              </NavLink>
+            </button>
+          </div>
+          {/* <div>
           <NavLink to='/users' exact={true} activeClassName='active'>
             Users
           </NavLink>
         </div> */}
-        <div>
-          <LogoutButton />
+          <div>
+            {user &&
+              <LogoutButton />
+            }
+          </div>
         </div>
       </div>
     </nav>
